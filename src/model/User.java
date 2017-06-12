f70ee1c222cb3d22eed3de3bb7ac8b5e17f5af7d
zackizf1204 <zackizf1204@gmail.com>
2017-06-12 22:52:31 +0700

    Update kelas User

--- src/model/User.java
+++ src/model/User.java
@@ -1,9 +1,27 @@
 package model;
 
+import java.net.HttpURLConnection;
+import java.util.ArrayList;
+
 /**
  * Created by Zacki Zulfikar Fauzi on 06-Jun-17.
  * NIM  : 13515147
  * File : User.java
  */
 public class User {
+  private HttpURLConnection connection = null;
+  private ArrayList<String> ListOfUser;
+  private String searchKey;
+  private int RepoMax;
+  private int RepoMin;
+
+
+  public User() {
+    ListOfUser = new ArrayList<>();
+  }
+
+  public User(String keyword) {
+    ListOfUser = new ArrayList<>();
+
+  }
 }
